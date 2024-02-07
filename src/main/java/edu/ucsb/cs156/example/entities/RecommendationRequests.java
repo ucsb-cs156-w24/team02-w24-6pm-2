d1154a9e9997f14120c3f1b08f5a8ff3efb9da8b
Thienan Vu <thienanvu@umail.ucsb.edu>
2024-02-06 20:45:54 -0800

    Created Database for Recommendations

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequests.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequests.java
@@ -1,7 +1,11 @@
 package edu.ucsb.cs156.example.entities;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +19,8 @@ import lombok.Builder;
 @Entity(name = "recommendationrequest")
 public class RecommendationRequests  {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String requesterEmail;
   private String professorEmail;
   private String Explanation;
